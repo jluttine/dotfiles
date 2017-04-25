bb3aabacfc93e1b67287b1eb0d3b12708b042624
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2017-04-25 20:26:08 +0300

    Fix GPG agent settings for KDE

--- .config/plasma-workspace/env/gpg-agent.sh
+++ .config/plasma-workspace/env/gpg-agent.sh
@@ -4,11 +4,5 @@
 # GnuPG 2.1.x changed the way the gpg-agent works, and that new approach no
 # longer requires (or even supports) the "start everything as a child of the
 # agent" scheme we've implemented in NixOS for older versions.
-#
-# To configure the gpg-agent for your X session, add the following code to
-# ~/.xsession or some other appropriate place that's sourced at start-up:
 
-gpg-connect-agent /bye
-GPG_TTY=$(tty)
-export GPG_TTY
-
+export GPG_AGENT_INFO=${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent:0:1
--- .config/plasma-workspace/env/ssh-agent.sh
+++
@@ -1,3 +0,0 @@
-# Use GPG for SSH keys
-unset SSH_AGENT_PID
-export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh"
