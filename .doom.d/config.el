203cfae59c767c4967dbb069bf36ca503e8fc6ae
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2023-01-18 16:18:21 +0200

    Switch to dark theme in Doom Emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -37,8 +37,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-solarized-light)
-;(setq doom-theme 'doom-solarized-dark-high-contrast)
+;(setq doom-theme 'doom-solarized-light)
+(setq doom-theme 'doom-solarized-dark-high-contrast)
 ;(setq doom-theme 'doom-one)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
