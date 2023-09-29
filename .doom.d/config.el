6a08820668cb5ac770a55eafe45df4d5a6c6c7d3
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2023-09-29 12:59:00 +0300

    Fix nerd symbols on doom emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -28,6 +28,7 @@
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 (setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 22))
+(setq nerd-icons-font-names '("SymbolsNerdFontMono-Regular.ttf"))
 
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
